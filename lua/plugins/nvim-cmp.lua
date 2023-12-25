84c13336055820630d94eec83917c9dedaa0f39b
Seb <@>
2023-12-25 16:15:26 +0100

    autocompletion with nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,58 +1,55 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip"
-  },
-  config = function()
-    -- local has_words_before = function()
-    --   unpack = unpack or table.unpack
-    --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    -- end
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim", -- vs-code like pictograms
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
 
-    local cmp = require('cmp')
-    local luasnip = require('luasnip')
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end
-      },
-      completion = {
-        autocomplete = false
-      },
-      mapping = cmp.mapping.preset.insert ({
-        -- ["<Tab>"] = cmp.mapping(function(fallback)
-        --   if cmp.visible() then
-        --     cmp.select_next_item()
-        --   elseif luasnip.expand_or_jumpable() then
-        --     luasnip.expand_or_jump()
-        --   elseif has_words_before() then
-        --     cmp.complete()
-        --   else
-        --     fallback()
-        --   end
-        -- end, { "i", "s" }),
-        -- ["<s-Tab>"] = cmp.mapping(function(fallback)
-        --   if cmp.visible() then
-        --     cmp.select_prev_item()
-        --   elseif luasnip.jumpable(-1) then
-        --     luasnip.jump(-1)
-        --   else
-        --     fallback()
-        --   end
-        -- end, { "i", "s" }),
-        -- ["<c-e>"] = cmp.mapping.abort(),
-        -- ["<CR>"] = cmp.mapping.confirm({ select=true }),
-      }),
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-      }
-    })
-  end
+		cmp.setup({
+			completion = {
+				completeopt = "menu,menuone,preview,noselect",
+				-- autocomplete = false,
+			},
+			-- configure lspkind for vs-code like pictograms in completion menu
+			formatting = {
+				format = lspkind.cmp_format({
+					maxwidth = 50,
+					ellipsis_char = "...",
+				}),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<c-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<c-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<c-b>"] = cmp.mapping.scroll_docs(-4),
+				["<c-f>"] = cmp.mapping.scroll_docs(4),
+				["<c-space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<c-e>"] = cmp.mapping.abort(), -- close completion window
+				["<cr>"] = cmp.mapping.confirm({ select = false }),
+			}),
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			-- sources for autocompletion
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- snippets
+				{ name = "buffer" }, -- text within current buffer
+				{ name = "path" }, -- file system paths
+			}),
+		})
+	end,
 }
--- lua/setup/general.lua
+++ lua/setup/general.lua
@@ -1,21 +1,22 @@
 -- vim.cmd.colorscheme "catppuccin-mocha"
-vim.cmd.colorscheme "badwolf"
+vim.cmd.colorscheme("badwolf")
 -- vim.cmd.colorscheme "cinnabar"
 
-vim.api.nvim_set_keymap('n', ',,', ':w<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', ',,', '<Esc>:w<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<LEADER><SPACE>', ':NvimTreeToggle<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<LEADER>ntff', ':NvimTreeFindFile<CR>', { noremap = true })
+vim.api.nvim_set_keymap("n", ",,", ":w<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", ",,", "<esc>:w<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader><space>", ":NvimTreeToggle<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>ntff", ":NvimTreeFindFile<CR>", { noremap = true })
 
-vim.api.nvim_set_keymap('i', '<C-Space>', '<C-n>', { noremap = true })
+-- todo: check if this is necessary anymore after configuring nvim-cmp
+-- vim.api.nvim_set_keymap("i", "<c-space>", "<C-n>", { noremap = true })
 
-vim.api.nvim_set_keymap('v', 'Y', ':OSCYankVisual<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'Y', ':OSCYankVisual<CR>', { noremap = true })
+vim.api.nvim_set_keymap("v", "Y", ":OSCYankVisual<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "Y", ":OSCYankVisual<CR>", { noremap = true })
 
 -- always join lines without the resulting whitespace space in between
-vim.api.nvim_set_keymap('n', 'J', 'gJi <ESC>diw', { noremap = true })
+vim.api.nvim_set_keymap("n", "J", "gJi <ESC>diw", { noremap = true })
 -- highlight but don't jump to next occurrence
-vim.api.nvim_set_keymap('n', '*', '*``', { noremap = true })
+vim.api.nvim_set_keymap("n", "*", "*``", { noremap = true })
 
 -- easy debugging snippet
 vim.cmd([[autocmd FileType python inoremap <C-v> import ipdb; ipdb.set_trace()]])
@@ -24,21 +25,21 @@ vim.cmd([[autocmd FileType ruby inoremap <C-b> require 'pry-byebug'; binding.pry
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-vim.cmd [[set number relativenumber]]
-vim.cmd [[syntax on]]
-vim.cmd [[set undofile]]
-vim.cmd [[set signcolumn=yes]]
-vim.cmd [[set cursorline cursorcolumn]]
-vim.cmd [[set nowrap]]
+vim.cmd([[set number relativenumber]])
+vim.cmd([[syntax on]])
+vim.cmd([[set undofile]])
+vim.cmd([[set signcolumn=yes]])
+vim.cmd([[set cursorline cursorcolumn]])
+vim.cmd([[set nowrap]])
 
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-left>', '<C-w>h', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-down>', '<C-w>j', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-up>', '<C-w>k', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-right>', '<C-w>l', { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-left>", "<C-w>h", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-down>", "<C-w>j", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-up>", "<C-w>k", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-right>", "<C-w>l", { noremap = true })
 
 -- configure folding through nvim_treesitter
 vim.opt.foldmethod = "expr"
