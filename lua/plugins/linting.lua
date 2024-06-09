f975ebec21ee3aa3d5401bce11ce4c09f060d47e
Seb <@>
2024-06-09 19:00:31 +0200

    configure eslint to be less annoying

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -4,6 +4,7 @@ return {
 	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
 	config = function()
 		local lint = require("lint")
+		local eslint = lint.linters.eslint_d
 
 		lint.linters_by_ft = {
 			javascript = { "eslint_d" },
@@ -11,6 +12,17 @@ return {
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			python = { "ruff" },
+		}
+
+		eslint.args = {
+			"--no-warn-ignored", -- <-- this is the key argument
+			"--format",
+			"json",
+			"--stdin",
+			"--stdin-filename",
+			function()
+				return vim.api.nvim_buf_get_name(0)
+			end,
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
