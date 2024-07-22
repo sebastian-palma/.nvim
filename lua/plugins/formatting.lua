53d62b59b4b952f4bc3725ba925a592140b1c9e3
Seb <@>
2024-07-22 10:30:51 +0200

    disable formatting for certain projects

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,37 +3,42 @@ return {
 	lazy = true,
 	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
 	config = function()
-		local conform = require("conform")
+		local current_dir = vim.fn.getcwd()
 
-		conform.setup({
-			formatters_by_ft = {
-				css = { "prettier" },
-				graphql = { "prettier" },
-				html = {},
-				javascript = { "prettier" },
-				javascriptreact = { "prettier" },
-				json = { "prettier" },
-				lua = { "stylua" },
-				markdown = { "prettier" },
-				python = { "isort", "black" },
-				svelte = { "prettier" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				yaml = { "prettier" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
+		-- Disable auto-formatting for certain projects.
+		if current_dir ~= "/mnt/quipuapp-front" then
+			local conform = require("conform")
 
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
+			conform.setup({
+				formatters_by_ft = {
+					css = { "prettier" },
+					graphql = { "prettier" },
+					html = {},
+					javascript = { "prettier" },
+					javascriptreact = { "prettier" },
+					json = { "prettier" },
+					lua = { "stylua" },
+					markdown = { "prettier" },
+					python = { "isort", "black" },
+					svelte = { "prettier" },
+					typescript = { "prettier" },
+					typescriptreact = { "prettier" },
+					yaml = { "prettier" },
+				},
+				format_on_save = {
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				},
 			})
-		end, { desc = "Format file or range (in visual mode)" })
+
+			vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			end, { desc = "Format file or range (in visual mode)" })
+		end
 	end,
 }
