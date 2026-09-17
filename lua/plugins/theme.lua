67f8a4d60e4c071b20de96225b802b0cd8d4ffc1
Seb <sebastianpalma@protonmail.com>
2026-09-17 11:28:11 +0200

    try gruvbox

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -17,16 +17,26 @@
 -- 	end,
 -- }
 
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {
+--     flavour = "macchiato", -- latte, frappe, macchiato, mocha
+--   },
+--   config = function(_, opts)
+--     require("catppuccin").setup(opts)
+--     vim.cmd.colorscheme "catppuccin"
+--   end,
+-- }
+
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false,
+  "ellisonleao/gruvbox.nvim",
   priority = 1000,
-  opts = {
-    flavour = "macchiato", -- latte, frappe, macchiato, mocha
-  },
-  config = function(_, opts)
-    require("catppuccin").setup(opts)
-    vim.cmd.colorscheme "catppuccin"
+  config = function()
+    vim.o.background = "light" -- or "dark" for dark mode
+    vim.cmd([[colorscheme gruvbox]])
   end,
+  opts = {},
 }
